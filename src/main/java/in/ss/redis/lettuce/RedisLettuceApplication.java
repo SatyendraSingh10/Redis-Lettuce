e37db52ddcf9340903e22d974ab044cd0164babd
SatyendraSingh <satyendra.singh3339@gmail.com>
2018-12-14 01:36:02 +0530

    Redis Lettuce with spring redis data

--- src/main/java/in/ss/redis/lettuce/RedisLettuceApplication.java
+++ src/main/java/in/ss/redis/lettuce/RedisLettuceApplication.java
@@ -1,14 +1,26 @@
 package in.ss.redis.lettuce;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 @SpringBootApplication
-public class RedisLettuceApplication {
+@Slf4j
+public class RedisLettuceApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RedisLettuceApplication.class, args);
-	}
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
 
+    public static void main(String[] args) {
+        SpringApplication.run(RedisLettuceApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("test key value : " + stringRedisTemplate.opsForValue().get("test"));
+    }
 }
 
